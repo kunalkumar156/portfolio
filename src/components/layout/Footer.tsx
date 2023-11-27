d572a4314a40bb11f199ac3a5cc5654ff187a14b
Kishor <yoyokunal156@gmail.com>
2023-11-27 12:18:26 +0530

    footer done

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Container, Box, Flex, Image, Link } from "@chakra-ui/react";
+import { Container, Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
@@ -8,29 +8,49 @@ function Footer() {
       <Box background="#F5FCFF" mt="3rem" mb="4rem">
         <Container maxW="1200" p="3rem 0rem">
           <Flex justify="space-between" alignItems="center" py={4} wrap="wrap">
-            <Image src="logo.svg" alt="logo" />
-            <Box>
+            <Image src="logo.svg" alt="logo" height={7} />
+            <Flex gap={5}>
               <Link _hover={{ textDecoration: "none" }}>Home</Link>
               <Link _hover={{ textDecoration: "none" }}>Portfolio</Link>
               <Link _hover={{ textDecoration: "none" }}>About Me</Link>
               <Link _hover={{ textDecoration: "none" }}>Contact</Link>
               <Link _hover={{ textDecoration: "none" }}>Testimonials</Link>
               <Link _hover={{ textDecoration: "none" }}>Portfolio</Link>
-            </Box>
+            </Flex>
 
-            <Box>
-              <Flex justify="center" align="center">
-                <FaInstagram size={30} />
-                <FaFacebook size={30} />
-                <FaTwitter size={30} />
-                <FaLinkedin size={30} />
-              </Flex>
-            </Box>
+            <Flex justify="center" align="center" gap={4}>
+              <FaInstagram size={23} />
+              <FaFacebook size={23} />
+              <FaTwitter size={23} />
+              <FaLinkedin size={23} />
+            </Flex>
           </Flex>
 
-          <Box height="2px" color="grey">
-            {" "}
-          </Box>
+          <hr
+            style={{
+              margin: "30px 0",
+              marginBottom: "30px",
+              width: "100%",
+              height: "2px",
+              backgroundColor: "rgba(40, 41, 56, 0.25)",
+            }}
+          />
+
+          <Flex alignItems="center" justify="space-between">
+            <Text fontSize="13px">Crafted with ❤️ by Kishor Kunal</Text>
+
+            <Flex align="center" gap={7}>
+              <Link fontSize="13px" _hover={{ textDecoration: "none" }}>
+                Privacy Policy
+              </Link>
+              <Link fontSize="13px" _hover={{ textDecoration: "none" }}>
+                Terms Of Services
+              </Link>
+              <Link fontSize="13px" _hover={{ textDecoration: "none" }}>
+                Cookies Settings
+              </Link>
+            </Flex>
+          </Flex>
         </Container>
       </Box>
     </>
