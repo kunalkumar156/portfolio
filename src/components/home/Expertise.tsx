7a49fb4c6842353ed0375d659598f845ad2fe906
Kishor <yoyokunal156@gmail.com>
2023-11-29 15:46:23 +0530

    responsive expertise

--- src/components/home/Expertise.tsx
+++ src/components/home/Expertise.tsx
@@ -16,10 +16,11 @@ function MySkills({
 }) {
   return (
     <Box
-      width="25%"
+      width={{ base: "100%", md: "45%", lg: "25%" }}
       bg="#F5FCFF"
-      padding="32px"
+      padding={{ base: "20px", md: "32px" }}
       borderRadius="10px"
+      mb={{ base: "20px", md: "0" }}
       _hover={{
         borderBottom: "2px solid #5E3BEE",
         transition: "border-bottom 0.1s ease, color 0.1s ease",
@@ -37,10 +38,20 @@ function MySkills({
       >
         <IconComponent />
       </Box>
-      <Text fontSize="22px" fontWeight="700" mt="3" mb="3" color="#282938">
+      <Text
+        fontSize={{ base: "18px", md: "22px" }}
+        fontWeight="700"
+        mt="3"
+        mb="3"
+        color="#282938"
+      >
         {heading}
       </Text>
-      <Text color="#1C1E53" fontWeight="600" fontSize="14px">
+      <Text
+        color="#1C1E53"
+        fontWeight="600"
+        fontSize={{ base: "12px", md: "14px" }}
+      >
         {para}
       </Text>
     </Box>
@@ -50,15 +61,25 @@ function MySkills({
 function Expertise() {
   return (
     <>
-      <Container maxW="1200" padding="3rem 0">
-        <Text fontWeight="600" color="#282938" pb="10px">
+      <Container maxW="1200" padding={{ base: "1rem", md: "3rem 0" }}>
+        <Text fontWeight="600" color="#282938" pb={{ base: "6", md: "10px" }}>
           My Skills
         </Text>
-        <Text color="#282938" fontSize="40px" fontWeight="700" pb="20px">
+        <Text
+          color="#282938"
+          fontSize={{ base: "32px", md: "40px" }}
+          fontWeight="700"
+          pb={{ base: "10px", md: "20px" }}
+        >
           My Expertise
         </Text>
 
-        <Box display="flex" justifyContent="space-around" gap="32px">
+        <Box
+          display="flex"
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent={{ base: "center", md: "left" }}
+          gap={{ base: "20px", md: "30px" }}
+        >
           <MySkills
             key="Javascript"
             IconComponent={FaJs}
